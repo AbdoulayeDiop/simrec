a75f5827a1ade3a6f282662d715ec7e2e600c3aa
Abdoulaye DIOP <adiop@solutiondatagroup.fr>
2024-05-03 20:00:45 +0200

    Change repository architecture

--- simrec.py
+++ simrec.py
@@ -1,6 +1,8 @@
 import pickle
 from meta_features import compute_meta_features
 import numpy as np
+from utils import get_valid_similarity_pairs
+from sklearn.preprocessing import minmax_scale
 
 def load_dataset(path):
     with open(path, "rb") as f:
@@ -12,11 +14,14 @@ def load_meta_model(path):
         meta_model_pipeline = pickle.load(f)
     return meta_model_pipeline
 
-def recommend(Xnum, Xcat, meta_model_pipeline, k=5):
+def recommend(Xnum, Xcat, meta_model_pipeline, k=5, include=None):
     meta_feature_vector = compute_meta_features(Xnum, Xcat)
     pred = meta_model_pipeline.predict([meta_feature_vector])[0]
     indices = np.argsort(-pred)
-    return list(zip(meta_model_pipeline.similarity_pairs[indices], pred[indices]))[:k]
+    res = list(zip(meta_model_pipeline.similarity_pairs[indices], pred[indices]))
+    if include is not None:
+        res = [val for val in res if val[0] in include]
+    return res[:k]
 
 if __name__ == "__main__":
     import argparse
@@ -26,6 +31,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     Xnum, Xcat = load_dataset(args.dataset)
+    Xnum = minmax_scale(Xnum)
     meta_model_pipeline = load_meta_model(args.model)
-    recommendation = recommend(Xnum, Xcat, meta_model_pipeline)
+    valid_similarity_pairs = get_valid_similarity_pairs(Xnum, Xcat)
+    recommendation = recommend(Xnum, Xcat, meta_model_pipeline, include=valid_similarity_pairs)
     print(recommendation)
